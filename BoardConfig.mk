20ce36db63ba5de4ca8f3b9c1b2071ead6547cb5
GtrCraft <nickvanbokhorst@hotmail.com>
2017-02-16 08:43:36 +0100

    USe 4.8 TC

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := addison_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
-#TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(PWD)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
 
 # Audio
 AUDIO_FEATURE_ENABLED_COMPRESS_VOIP := true
